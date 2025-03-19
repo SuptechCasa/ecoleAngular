69b769e5f716e76fa62fe2bc32231977dd93dc8f
enaanai <aenaanai@uae.ac.ma>
2025-03-19 13:52:29 +0000

    addphoto

--- src/app/etudiant/form/form.component.ts
+++ src/app/etudiant/form/form.component.ts
@@ -11,27 +11,42 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrl: './form.component.css'
 })
 export class FormComponent {
-formData=this.fb.group({
+etudiantData=this.fb.group({
   id:['',Validators.required],
   nom:['',Validators.required],
   age:['',Validators.required]
 })
-
+selectedFile:any
 etudiant:any
 action="Ajouter"
 constructor(private activeModal:NgbActiveModal,private fb:FormBuilder,private etudiantService:EtudiantService){}
 
 actionEtudiant(){
   if (this.action==="Ajouter"){
-  this.etudiantService.addEtudiant(this.formData.value)
+  const id=this.etudiantData.get('id')?.value
+  const nom=this.etudiantData.get('nom')?.value
+  const age=this.etudiantData.get('age')?.value
+  const formData=new FormData()
+  if (id) formData.append('id',id)
+  if (nom) formData.append('nom',nom)
+  if (age) formData.append('age',age)
+  formData.append('photo',this.selectedFile)
+
+  this.etudiantService.addEtudiant(formData)
   }else{
-    this.etudiantService.updateEtudiant(this.formData.value)
+    this.etudiantService.updateEtudiant(this.etudiantData.value)
   }
   this.activeModal.close()
 }
 ngOnInit() {
   if (this.action==="Modifier"){
-    this.formData.setValue(this.etudiant)
+    this.etudiantData.setValue(this.etudiant)
   }
+}
+
+selectFile(event:Event){
+  const fileInput=event.target as HTMLInputElement
+  if (fileInput.files&&fileInput.files.length)
+  this.selectedFile=fileInput.files[0]
 }
 }
